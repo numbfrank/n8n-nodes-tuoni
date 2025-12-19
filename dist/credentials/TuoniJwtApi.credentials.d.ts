6a429135269299b8114521dc3635dbf997538aa4
numb frank <gazlockwood@gmail.com>
2025-12-19 18:33:15 -0500

    update build

--- dist/credentials/TuoniJwtApi.credentials.d.ts
+++ dist/credentials/TuoniJwtApi.credentials.d.ts
@@ -1,11 +1,13 @@
-import type { IAuthenticate, Icon, ICredentialDataDecryptedObject, ICredentialTestRequest, ICredentialType, IDataObject, IHttpRequestHelper, INodeProperties } from 'n8n-workflow';
+import type { Icon, ICredentialDataDecryptedObject, ICredentialTestRequest, ICredentialType, IHttpRequestHelper, IHttpRequestOptions, INodeProperties } from 'n8n-workflow';
 export declare class TuoniJwtApi implements ICredentialType {
     name: string;
     displayName: string;
     icon: Icon;
     documentationUrl: string;
     properties: INodeProperties[];
-    preAuthentication: (this: IHttpRequestHelper, credentials: ICredentialDataDecryptedObject) => Promise<IDataObject>;
-    authenticate: IAuthenticate;
+    preAuthentication(this: IHttpRequestHelper, credentials: ICredentialDataDecryptedObject): Promise<{
+        token: string;
+    }>;
+    authenticate(credentials: ICredentialDataDecryptedObject, requestOptions: IHttpRequestOptions): Promise<IHttpRequestOptions>;
     test: ICredentialTestRequest;
 }
--- dist/credentials/TuoniJwtApi.credentials.js
+++ dist/credentials/TuoniJwtApi.credentials.js
@@ -42,41 +42,13 @@ class TuoniJwtApi {
                 displayName: 'Token',
                 name: 'token',
                 type: 'hidden',
-                typeOptions: { password: true },
+                typeOptions: {
+                    expirable: true,
+                    password: true,
+                },
                 default: '',
-                description: 'JWT token (automatically populated by preAuthentication)',
             },
         ];
-        this.preAuthentication = async function (credentials) {
-            var _a, _b;
-            const token = (await this.helpers.httpRequest({
-                baseURL: credentials.serverUrl,
-                url: '/api/v1/auth/login',
-                method: 'POST',
-                auth: {
-                    username: String((_a = credentials.username) !== null && _a !== void 0 ? _a : ''),
-                    password: String((_b = credentials.password) !== null && _b !== void 0 ? _b : ''),
-                },
-                headers: {
-                    Accept: 'text/plain',
-                },
-                json: false,
-                skipSslCertificateValidation: Boolean(credentials.ignoreSSL),
-            }));
-            return { ...credentials, token };
-        };
-        this.authenticate = async (credentials, requestOptions) => {
-            var _a, _b;
-            const next = { ...requestOptions };
-            next.headers = { ...((_a = next.headers) !== null && _a !== void 0 ? _a : {}) };
-            next.skipSslCertificateValidation = Boolean(credentials.ignoreSSL);
-            const token = String((_b = credentials.token) !== null && _b !== void 0 ? _b : '');
-            if (token) {
-                next.headers.Authorization = `Bearer ${token}`;
-                delete next.auth;
-            }
-            return next;
-        };
         this.test = {
             request: {
                 baseURL: '={{$credentials?.serverUrl}}',
@@ -88,6 +60,36 @@ class TuoniJwtApi {
                 json: true,
                 skipSslCertificateValidation: '={{$credentials?.ignoreSSL}}',
             },
+        };
+    }
+    async preAuthentication(credentials) {
+        var _a, _b;
+        const token = (await this.helpers.httpRequest({
+            baseURL: credentials.serverUrl,
+            url: '/api/v1/auth/login',
+            method: 'POST',
+            auth: {
+                username: String((_a = credentials.username) !== null && _a !== void 0 ? _a : ''),
+                password: String((_b = credentials.password) !== null && _b !== void 0 ? _b : ''),
+            },
+            headers: {
+                Accept: 'text/plain',
+            },
+            json: false,
+            skipSslCertificateValidation: Boolean(credentials.ignoreSSL),
+        }));
+        return { token };
+    }
+    async authenticate(credentials, requestOptions) {
+        var _a;
+        const token = String((_a = credentials.token) !== null && _a !== void 0 ? _a : '');
+        return {
+            ...requestOptions,
+            headers: {
+                ...requestOptions.headers,
+                Authorization: `Bearer ${token}`,
+            },
+            skipSslCertificateValidation: Boolean(credentials.ignoreSSL),
         };
     }
 }
